5edaee198e77b1d19f765b5a651c8d71dbed40b3
felipealvx <felipealvk@gmail.com>
2025-09-23 23:07:40 -0300

    finalização das perguntas do formulário

--- app/form/finish.tsx
+++ app/form/finish.tsx
@@ -1,23 +1,26 @@
 import { useQuestForm } from "@/hooks/useQuestForm";
 import { ScrollView, Text, View } from "react-native";
 import { styles } from "@/styles/finish";
+import { Button } from "@/components/form/Button";
 
 export default function StepFour() {
   const { questFormData } = useQuestForm();
   return (
     <ScrollView style={styles.container}>
       <View style={styles.congratulations}>
-        <Text> 
-          PARABÉNS
+        <Text style={styles.congratulationsText}> 
+          PARABÉNS 👋
         </Text>
-        <Text>
-          {questFormData.fullName || 'Usuário'}!
+        <Text style={styles.congratulationsUser}>
+          {questFormData.fullName || 'FELIPE ALVES DA SILVA'}
         </Text>
       </View>
-      <Text>
-        Você concluiu o formulário!
-        Exporte os dados para uma planilha EXCEL clicando no botão abaixo, ou salve em uma turma.
-      </Text>
+      <View>
+        <Text style={styles.subtitle}>
+          Você concluiu o formulário!
+          Exporte os dados para uma planilha EXCEL clicando no botão abaixo, ou salve em uma turma.
+        </Text>
+      </View>
       <Text>
         Dados completos do formulário: {JSON.stringify(questFormData, null, 2)}
       </Text>
--- styles/finish.ts
+++ styles/finish.ts
@@ -1,10 +1,34 @@
+import { Colors } from "@/constants/Colors";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create ({
   container: {
-    
+    padding: 16,
   },
   congratulations: {
-    
+    padding: 8,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#c5c5cfff'
+  },
+  congratulationsText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.secondary,
+  },
+  congratulationsUser: {
+    fontSize: 34,
+    color: Colors.deepBlue,
+    marginTop: 4,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  subtitle: {
+    fontSize: 16,
+    marginBottom: 16,
+    textAlign: 'center',
+    color: Colors.text,
   }
 })
